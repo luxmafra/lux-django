2a25a7a7fd41a50e3be2df1dc131a1ad53d3ad85
luxmafra <luxmafra@gmail.com>
2020-10-24 11:07:58 +0100

    settings on collect static files (flake8)

--- pypro/settings.py
+++ pypro/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dj_database_url
 from decouple import config, Csv
-from django.conf.global_settings import STATIC_ROOT, MEDIA_URL
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
