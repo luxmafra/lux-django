18bb22e5a62e2af8237874088114a1f752fbc753
luxmafra <luxmafra@gmail.com>
2020-10-24 11:00:42 +0100

    settings on collect static files.

--- pypro/settings.py
+++ pypro/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import dj_database_url
 from decouple import config, Csv
+from django.conf.global_settings import STATIC_ROOT, MEDIA_URL
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
