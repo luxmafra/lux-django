45f5e1158e71ad5b83e978659aa68104aa707c7a
luxmafra <luxmafra@gmail.com>
2020-11-19 17:53:37 +0000

    aws s3 region name

--- pypro/settings.py
+++ pypro/settings.py
@@ -146,6 +146,8 @@ if AWS_ACCESS_KEY_ID:
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
 
+    AWS_S3_REGION_NAME = 'eu-west-2'
+
     AWS_S3_SIGNATURE_VERSION = "s3v4"
 
     AWS_DEFAULT_ACL = 'public-read'
