cfb370d2d295cf8eb9976b85aace4f8585b9fd80
luxmafra <luxmafra@gmail.com>
2020-11-24 10:26:41 +0000

    template block

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -14,7 +14,7 @@ def test_status_code(resp):
 
 
 def test_titulo_video(resp):
-    assert_contains(resp, '<h1>Video: Gladiator</h1>')
+    assert_contains(resp, 'Video: Gladiator')
 
 
 def test_conteudo_video(resp):
